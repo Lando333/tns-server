c3a8d70eadc1e5a16afec067744d8b106a979f68
Ryan Rojas <landoslair@gmail.com>
2023-06-17 19:28:14 -0400

    removed SQL echo

--- server/config.py
+++ server/config.py
@@ -2,14 +2,13 @@ from dotenv import load_dotenv
 import os
 import redis
 
-
 load_dotenv()
 
 class ApplicationConfig:
     SECRET_KEY = os.environ["SECRET_KEY"]
 
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-    SQLALCHEMY_ECHO = True
+    # SQLALCHEMY_ECHO = True
     SQLALCHEMY_DATABASE_URI = r"sqlite:///app.db"
 
     SESSION_TYPE = "redis"
