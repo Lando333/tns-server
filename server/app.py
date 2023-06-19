1d4d256cd6e6c62afe0f3b51095efe45cf405dd2
Ryan Rojas <landoslair@gmail.com>
2023-06-18 20:09:08 -0400

    updated flask_cors

--- server/app.py
+++ server/app.py
@@ -20,8 +20,8 @@ app.json.compact = False
 migrate = Migrate(app, db)
 # Hashes password
 bcrypt = Bcrypt(app)
-CORS(app, supports_credentials=True)
 server_session = Session(app)
+CORS(app, supports_credentials=True)
 db.init_app(app)
 
 api = Api(app)
@@ -31,6 +31,19 @@ api = Api(app)
 def index():
     return 'Welcome to Tao Now Solutions!'
 
+
+@app.route("/@me")
+def get_current_user():
+    user_id = session.get("user_id")
+
+    if not user_id:
+        return jsonify({"error": "Unauthorized"}), 401
+    
+    user = User.query.filter_by(id=user_id).first()
+    return jsonify({
+        "id": user.id,
+        "email": user.email
+    }) 
 
 @app.route("/register", methods=["POST"])
 def register_user():
@@ -61,6 +74,31 @@ def register_user():
         "email": new_user.email
     }), 201
 
+@app.route("/login", methods=["POST"])
+def login_user():
+    email = request.json["email"]
+    password = request.json["password"]
+
+    user = User.query.filter_by(email=email).first()
+
+    if user is None:
+        return jsonify({"error": "Unauthorized"}), 401
+
+    if not bcrypt.check_password_hash(user.password, password):
+        return jsonify({"error": "Unauthorized"}), 401
+    
+    session["user_id"] = user.user_id
+
+    return jsonify({
+        "user_id": user.user_id,
+        "email": user.email
+    })
+
+@app.route("/logout", methods=["POST"])
+def logout_user():
+    session.pop("user_id")
+    return "200"
+
 
 @app.route("/delete/<string:user_id>", methods=["DELETE"])
 def delete_user(user_id):
@@ -72,7 +110,7 @@ def delete_user(user_id):
     db.session.delete(user)
     db.session.commit()
 
-    return jsonify({"message": "User deleted successfully"})
+    return jsonify({"message": "User deleted successfully"}), 202
 
 
 
