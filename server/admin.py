23fca7703e33c22b6e1233f4563f736be35889df
Ryan Rojas <landoslair@gmail.com>
2023-06-28 18:44:02 -0400

    added commands for easy editing

--- server/admin.py
+++ server/admin.py
@@ -14,18 +14,44 @@ from models import db, User, Address, Therapist, Service, Appointment,\
     therapist_services, user_appointment
 
 
+OFFERED_SERVICES = [
+    'Swedish',
+    'Deep Tissue',
+    'Reflexology',
+    'Acupuncture',
+    'Acupressure',
+    'Reiki'
+]
+
 if __name__ == '__main__':
-    # fake = Faker()
+    fake = Faker()
     # now = datetime.now()
 
     with app.app_context():
         print("--Starting admin..--")
 
         # How to delete an object:
-        therapist = Therapist.query.filter_by(therapist_id=5).first()
-        if therapist:
-            db.session.delete(therapist)
-            db.session.commit()
+        # therapist = Therapist.query.filter_by(therapist_id=4).first()
+        # if therapist:
+        #     db.session.delete(therapist)
+        #     db.session.commit()
+
+        # Adding services to therapist:
+        # services = Service.query.all()
+        # therapist = Therapist.query.filter_by(therapist_id=4).first()
+        # if therapist:
+        #     for service in services:
+        #         therapist.services.append(service)
+        #     db.session.add(therapist)
+        #     db.session.commit()
+
+        # Editing object values
+        # ryan = User.query.filter_by(user_id="b0f0e3f1d0a3413eacfcc21b7a9dd05d").first()
+        # ryan.first_name = 'Ryan'
+        # ryan.last_name = 'Rojas'
+        # ryan.email = 'ryan@gmail.com'
+        # db.session.add(ryan)
+        # db.session.commit()
 
 
 
